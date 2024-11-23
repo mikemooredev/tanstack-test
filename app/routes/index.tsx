2092dd682c6949c00484153b697bb11058645d8f
Mike <me@mikemoore.dev>
2024-11-23 21:52:28 +0000

    Comment out references to file system module
    Cloudflare pages doesn't support the fs module so we need to remove it

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,16 @@
-// app/routes/index.tsx
-import * as fs from 'node:fs'
+//import * as fs from 'node:fs'
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/start'
 
-const filePath = 'count.txt'
+//const filePath = 'count.txt'
 
 async function readCount() {
+  return 10;
+  /*
   return parseInt(
     await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
   )
+  */
 }
 
 const getCount = createServerFn({
@@ -20,8 +22,10 @@ const getCount = createServerFn({
 const updateCount = createServerFn({ method: 'POST' })
   .validator((d: number) => d)
   .handler(async ({ data }) => {
+    /*
     const count = await readCount()
     await fs.promises.writeFile(filePath, `${count + data}`)
+    */
   })
 
 export const Route = createFileRoute('/')({
